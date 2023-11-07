7aa05ea9973f21ccbbb88240db434bd7fed3747a
Jakob Güttinger <jakob.guettinger@hs-augsburg.de>
2023-11-07 15:33:19 +0100

    fix broker connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,98 @@
-// INCLUDES
-#include <Arduino.h>
-#include <PubSubClient.h>
 #include <ESP8266WiFi.h>
-#include <OneWire.h>
+#include <Ticker.h>
+#include <AsyncMqttClient.h>
 #include <WiFiManager.h>
-#include <ArduinoJson.h>
 #include <Credentials.h>
 
-// DEFINES
 
-char location_id[8];
+const char* topic =   "Noe1337";
 
-WiFiClientSecure espClient;
-PubSubClient client(espClient);
+String clientId = "WeMos_";
+char clientID_c_str[30] = "WeMos_";
 
-// CODE
+const int output = LED_BUILTIN;
+char message[50];
+
+AsyncMqttClient mqttClient;
+Ticker mqttReconnectTimer;
+Ticker wifiReconnectTimer;
+
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
+
+
 void setup_wifi()
 {
   delay(10);
   WiFiManager wifiManager;
   // configure WiFiManager to include fields for Location, Room and Project IDs
-  WiFiManagerParameter custom_ids("server", "mqtt server", location_id, 40);
-  wifiManager.addParameter(&custom_ids);
+  // WiFiManagerParameter custom_ids("server", "mqtt server", location_id, 40);
+  // wifiManager.addParameter(&custom_ids);
 
   wifiManager.autoConnect("IntellisensorySetup");
 }
 
 
-void reconnect()
-{
-  while (!client.connected())
-  {
-    Serial.print("Attempting MQTT connection...");
-    
-    String client_id_str = "Intellisensory" + String(random(0xffff), HEX);
-    char* client_id = (char*) client_id_str.c_str();
-    if (client.connect(client_id))
-    {
-      Serial.println(" Mqtt connected");
-    }
-    else
-    {
-      Serial.println(" try again in 5 seconds");
-      delay(5000);
-    }
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.setClientId(clientID_c_str);
+  mqttClient.connect();
+}
+
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+  if (WiFi.isConnected()) {
+    mqttReconnectTimer.once(2, connectToMqtt);
   }
 }
 
 
-void setup()
-{
-  Serial.begin(9600);
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  Serial.println("Publish received.");
+}
+
+
+long long int macToInt(const uint8_t* mac){
+  long long int result = 1;
+  for (int i = 0; i < 6; ++i) {
+    result *= mac[i];
+  }
+  return result;
+}
+
+void composeClientID() {
+  uint8_t mac[6];
+  WiFi.macAddress(mac);                 //MAC Addr lesen
+  long long int macInt = macToInt(mac); //MAC als int
+  char buffer1[20];                     //MAC als C_string
+  itoa(macInt,buffer1,16);
+  strcat(clientID_c_str, buffer1);      //clientID erstellen
+  Serial.println(clientID_c_str);
+}
+
+void setup() {
+  Serial.begin(115200);
+  composeClientID() ;
+
   setup_wifi();
-  client.setServer(MQTT_BROKER, MQTT_PORT);
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+
+  mqttClient.onMessage(onMqttMessage);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+
+  connectToMqtt();
 }
 
-
-void loop()
-{
-  if (!client.connected())
-  {
-    reconnect();
-  }
-  client.loop();
-  delay(1000);
-  client.publish("Noe1337", "Hallo Welt");
+unsigned long int newTime, lastTime, period = 10000;
+void loop() {
+  mqttClient.publish(topic, 0, false, "test 2");
+  delay(100);
 }
