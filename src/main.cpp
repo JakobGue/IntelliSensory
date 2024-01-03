a37874575da228abd7ca59c9d11dcb6e44f4b670
Jakob Güttinger <jakob.guettinger@hs-augsburg.de>
2024-01-03 17:34:05 +0100

    adapt pubsub client

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,17 @@
-// Includes
 #include <Arduino.h>
+#include <PubSubClient.h>
 #include <ESP8266WiFi.h>
-#include <Ticker.h>
-#include <AsyncMqttClient.h>
-#include <WiFiManager.h>
 #include <Credentials.h>
+#include <DateTime.h>
 #include <DallasTemperature.h>
 #include <OneWire.h>
 #include <Wire.h>
 #include <SensirionI2CScd4x.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
-#include <DateTime.h>
-#include <iostream>
-#include <vector>
-#include <tuple>
 #include "helperfunction.h"
+#include "WiFiManager.h"
 
-
-
-//Defines
-
-const char* topic =   "Noe1337";
-String clientId = "WeMos_";
-char clientID_c_str[30] = "WeMos_";
-
-
-
-
-AsyncMqttClient mqttClient;
-Ticker mqttReconnectTimer;
-Ticker wifiReconnectTimer;
-
-WiFiEventHandler wifiConnectHandler;
-WiFiEventHandler wifiDisconnectHandler;
-
-
-
-const int output = LED_BUILTIN;
 char message[50];
 const int sampleWindow = 50;
 unsigned int sample;
@@ -50,110 +24,77 @@ DallasTemperature temp_sensor(&oneWire);
 SensirionI2CScd4x scd4x;
 
 
+int i = 0;
 
+const char* ssid     = SSID ;
+const char* password = WIFI_PWD;
+const char* server = MQTT_HOST;
 
-// Code
+const String location = LOCATION;
+const String room = ROOM;
+const float sound_threshold = 1.2;
+const String clientId = location + "_IntelliSensory_" + room;
+
+float sound_cycle[120];
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  // handle message arrived
+}
+WiFiClientSecure espClient;
+PubSubClient client(server, 15, callback, espClient);
+
 void setupDateTime(){
-  DateTime.setServer("de.pool.ntp.org");
-  DateTime.setTimeZone("UTC");
+  DateTime.setServer("time1.hs-augsburg.de", "time2.hs-augsburg.de", "time3.hs-augsburg.de");
+  DateTime.setTimeZone("UTC-1");
   DateTime.begin();
+  Serial.println(DateTime.toISOString());
 }
 
-
-void setup_wifi()
+void reconnect() 
 {
-  delay(10);
-  Serial.println("Connecting to WiFi");
-  WiFiManager wifiManager;
-  // configure WiFiManager to include fields for Location, Room and Project IDs
-  // WiFiManagerParameter custom_ids("server", "mqtt server", location_id, 40);
-  // wifiManager.addParameter(&custom_ids);
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "IntelliSensory_" + location + "_" + room + "_" + MODE;
 
-  wifiManager.autoConnect("IntellisensorySetup");
-}
-
-
-void connectToMqtt() {
-  Serial.println("Connecting to MQTT...");
-  mqttClient.setClientId(clientID_c_str);
-  if (digitalRead(DEBUG_PIN) == HIGH){
-    mqttClient.setCredentials(DEBUG_USER, DEBUG_PASSWORD);
-  }
-  else{
-    mqttClient.setCredentials(MQTT_USER, MQTT_PASSWORD);
-  }
-  mqttClient.connect();
-}
-
-
-void onMqttConnect(bool sessionPresent) {
-  Serial.println("Connected to MQTT.");
-  Serial.print("Session present: ");
-  Serial.println(sessionPresent);
-  if (sessionPresent == 0){
-    Serial.println("Keine Internetverbindung");
+    if (client.connect(clientId.c_str(), MQTT_USER, MQTT_PASSWORD)) {
+      Serial.println(" Mqtt connected");
+    }else {
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
   }
 }
 
-void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
-  Serial.println("Disconnected from MQTT.");
-  if (WiFi.isConnected()) {
-    mqttReconnectTimer.once(2, connectToMqtt);
-  }
-}
-
-
-void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
-  Serial.println("Publish received.");
-}
-
-
-long long int macToInt(const uint8_t* mac){
-  long long int result = 1;
-  for (int i = 0; i < 6; ++i) {
-    result *= mac[i];
-  }
-  return result;
-}
-
-void composeClientID() {
-  uint8_t mac[6];
-  WiFi.macAddress(mac);                 //MAC Addr lesen
-  long long int macInt = macToInt(mac); //MAC als int
-  char buffer1[20];                     //MAC als C_string
-  itoa(macInt,buffer1,16);
-  strcat(clientID_c_str, buffer1);      //clientID erstellen
-  Serial.println(clientID_c_str);
-}
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
+  delay(10);
+  Serial.println('\n');
   pinMode(DEBUG_PIN, INPUT);
-  composeClientID() ;
-  setup_wifi();
-  while (WiFi.status() != WL_CONNECTED){
+  
+  WiFi.begin(ssid, password);  
+  espClient.setInsecure();           // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid); Serial.println(" ...");
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
     delay(1000);
-    Serial.println("Connecting to Wifi...");
-  }
-  //Serial.println("ERREICHT");
-  mqttClient.onConnect(onMqttConnect);
-  mqttClient.onDisconnect(onMqttDisconnect);
-
-  mqttClient.onMessage(onMqttMessage);
-  if (digitalRead(DEBUG_PIN) == HIGH){
-    mqttClient.setServer(DEBUG_HOST, DEBUG_PORT);
-  }
-  else{
-    mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+    Serial.print(++i); Serial.print(' ');
   }
 
-  connectToMqtt();
+  Serial.println('\n');
+  Serial.println("Connection established!");  
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());  
+  if (client.connect(clientId.c_str(), MQTT_USER, MQTT_PASSWORD)) {
+    Serial.println("connected");
+  }
 
-  setupDateTime();
-  
-  pinMode(SENSOR_PIN, INPUT);
-  
   Wire.begin();
+  setupDateTime();
+  Serial.println(DateTime.toISOString());
+
   if (MODE == "DESK") {
     scd4x.begin(Wire);
 
@@ -168,6 +109,7 @@ void setup() {
 
     // Start Measurement
     error = scd4x.startPeriodicMeasurement();
+
     if (error) {
         Serial.print("Error trying to execute startPeriodicMeasurement(): ");
         errorToString(error, errorMessage, 256);
@@ -180,13 +122,13 @@ void setup() {
 
 float read_temp_heater(){
   float retval;
+  retval = 1.0;
   temp_sensor.requestTemperatures();
   retval = temp_sensor.getTempCByIndex(0);
   return retval;
 }
 
 float read_loudness(){
-  float retval;
   unsigned long startMillis= millis();                   // Start of sample window
   float peakToPeak = 0;                                  // peak-to-peak level
  
@@ -196,7 +138,7 @@ float read_loudness(){
                                                           // collect data for 50 mS
   while (millis() - startMillis < sampleWindow)
   {
-      sample = analogRead(SENSOR_PIN);                    //get reading from microphone
+      sample = analogRead(SENSOR_PIN);            //get reading from microphone
       if (sample < 1024)                                  // toss out spurious readings
       {
          if (sample > signalMax)
@@ -208,110 +150,69 @@ float read_loudness(){
             signalMin = sample;                           // save just the min levels
          }
       }
-      //Serial.print("Lautstärke: ");
-      //Serial.println(sample)
    }
  
-  peakToPeak = signalMax - signalMin;                    // max - min = peak-peak amplitude
-  int db = map(peakToPeak,20,900,49.5,90);             //calibrate for deciBels
-  retval = db;
-  return retval;
+  peakToPeak = signalMax - signalMin;  
+  float signal = ((peakToPeak * 5.0) / 1024);                 // max - min = peak-peak amplitude
+  return signal;
 }
 
 
-
-//unsigned long int newTime, lastTime, period = 10000;
-void loop() {
-  String result = "";
-  if (MODE == "DESK") {
-    uint16 co2;
-    float temperature_room, humidity;
-    unsigned long startMillis= millis();                   // Start of sample window
-    float peakToPeak = 0;                                  // peak-to-peak level
-  
-    unsigned int signalMax = 0;                            //minimum value
-    unsigned int signalMin = 1024;                         //maximum value
-
-                                                            // collect data for 50 mS
-    while (millis() - startMillis < sampleWindow)
-    {
-        sample = analogRead(SENSOR_PIN);                    //get reading from microphone
-        //Serial.println(sample);
-        if (sample < 1024)                                  // toss out spurious readings
-        {
-          if (sample > signalMax)
-          {
-              signalMax = sample;                           // save just the max levels
-          }
-          else if (sample < signalMin)
-          {
-              signalMin = sample;                           // save just the min levels
-          }
-        }
-    }
- 
-    peakToPeak = signalMax - signalMin;                    // max - min = peak-peak amplitude
-    int db = map(peakToPeak,20,900,49.5,90); 
-    
-    sample = analogRead(SENSOR_PIN);
-    scd4x.readMeasurement(co2,temperature_room , humidity);
-
-    String loudness_str = String(db)+ " db";
-    
-    String sample_str = String(sample);
-    String co2_str = String(co2) + " ppm";
-    String temperature_room_str = String(temperature_room) + " °C";
-    String humidity_str = String(humidity) + " %";
-
-    String formattedDate = DateTime.toISOString();
-  
-    const SensorData sensorData[] = {
-          {1, db, formattedDate},
-          {2, temperature_room, formattedDate},
-          {3, co2, formattedDate},
-          {4, humidity, formattedDate}
-      };
-    result = createJson(false, sensorData, sizeof(sensorData) / sizeof(sensorData[0]));
-
-    Serial.print("Kohlenstoffdioxid: ");
-    Serial.print("\t");
-    Serial.println(co2_str);
-    Serial.print("Raumtemperatur: ");
-    Serial.print("\t");
-    Serial.println(temperature_room_str);
-    Serial.print("Luftfeuchte: ");
-    Serial.print("\t");
-    Serial.print("\t");
-    Serial.println(humidity_str);
-    Serial.print("Lautstärke: ");
-    Serial.print("\t");
-    Serial.print("\t");
-    Serial.println(sample_str);
-    Serial.print("Temperatur an Heizung: ");
-    Serial.print("\t");
-  }else {
-    float temperature_heater = read_temp_heater();
-    String temperature_heater_str = String(temperature_heater) + " °C";
-    String formattedDate = DateTime.toISOString();
-  
-    const SensorData sensorData[] = {
-          {1, temperature_heater, formattedDate}
-      };
-
-    result = createJson(false, sensorData, sizeof(sensorData) / sizeof(sensorData[0]));
-    Serial.println(temperature_heater_str);
+void loop()
+{
+  if (!client.connected()) {
+    reconnect();
   }
+  client.loop();
+  Serial.println(i);
+  if (i == 120) {
 
-  mqttClient.publish(topic, 0, false, result.c_str());
+    String result = "";
+    if (MODE == "DESK") {
+      uint16 co2;
+      float temperature_room, humidity;
+      scd4x.readMeasurement(co2, temperature_room, humidity);
+      float currentMax = 0;
+      for (int i = 0; i < (sizeof(sound_cycle) / sizeof(sound_cycle[0])); i++) {
+        if (sound_cycle[i] > currentMax) {
+          currentMax = sound_cycle[i];
+        }
+      }
+      Serial.println(currentMax);
+      bool sound = (currentMax >= sound_threshold);
+      String formattedDate = DateTime.toISOString();
+    
+      const SensorData sensorData[] = {
+            {5, sound, formattedDate},
+            {3, temperature_room, formattedDate},
+            {1, co2, formattedDate},
+            {4, humidity, formattedDate}
+        };
+      result = createJson(false, sensorData, sizeof(sensorData) / sizeof(sensorData[0]));
+      Serial.println(result);
+      Serial.println(sizeof(result.c_str()));
+    }else {
+      float temperature_heater = read_temp_heater();
+      String temperature_heater_str = String(temperature_heater) + " °C";
+      String formattedDate = DateTime.toISOString();
+    
+      const SensorData sensorData[] = {
+            {1, temperature_heater, formattedDate}
+        };
+      
+      result = createJson(false, sensorData, sizeof(sensorData) / sizeof(sensorData[0]));
+      Serial.println(result.c_str());
+    }
 
-  Serial.print("Datum:");
-  Serial.print("\t");
-  Serial.print("\t");
-  Serial.print("\t");
-  Serial.println(DateTime.toISOString());
-  
-  Serial.println("************************************************");
+    String topic = "se/i2projekt/" + location + "/" + room + "/intellisensory";
+    Serial.println(topic);
+    client.publish(topic.c_str(), result.c_str());
 
-  delay(5000);
+    i = 0;
+    }
+
+  // delaying whole 60s at once results in connection loss, therefore delay 500ms 120 times
+  delay(450);
+  sound_cycle[i] = read_loudness();
+  i++;
 }
-
